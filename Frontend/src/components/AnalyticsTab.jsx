9eabfdcf8743855edaa592c16dc72b1bde11568b
Raj Meena <heyrajmeena@gmail.com>
2024-12-26 17:08:14 +0530

    change remote repo

--- Frontend/src/components/AnalyticsTab.jsx
+++ Frontend/src/components/AnalyticsTab.jsx
@@ -113,9 +113,12 @@ const AnalyticsCard = ({ title, value, icon: Icon, color }) => (
 				<h3 className='text-white text-3xl font-bold'>{value}</h3>
 			</div>
 		</div>
+		
 		<div className='absolute inset-0 bg-gradient-to-br from-emerald-600 to-emerald-900 opacity-30' />
 		<div className='absolute -bottom-4 -right-4 text-emerald-800 opacity-50'>
 			<Icon className='h-32 w-32' />
 		</div>
 	</motion.div>
 );
+
+
